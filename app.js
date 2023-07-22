c20293b405a256e94a6719b6a4aaf0254b8e498f
Graham Galloway <Grahamgalloway0@gmail.com>
2023-07-22 15:01:01 -0400

    Finished the exercises

--- app.js
+++ app.js
@@ -3,8 +3,53 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
+//**I commented out the simpler code so the more complex code would run**
+
 // Exercise 1
+//const firstName;
+//let lastName;
+var age;
 
 // Exercise 2
+//const firstName ="Graham";
+//let lastName ="Galloway"
+var age = 29;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+// Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+
+//Exercise 1
+const firstName ="Graham";
+let lastName ="Galloway"
+var age = 29;
+
+//Exercise 2
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+//let fullName = `${firstName} ${lastName}`;
+console.log(fullName);
+
+
+//Exercise 3
+let city = "Orlando";
+let favoritePastime = "Playing Video Games"
+let myStory = `Hello, my name is ${fullName}. I live in ${city}. I really enjoy ${favoritePastime}.`;
+console.log(myStory);
